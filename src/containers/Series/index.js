ae798d32aece112f1516d8899ea62817aa6bd565
umitcanbal <umitcanbal@gmail.com>
2020-03-21 20:53:54 +0100

    conditional rendering done

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -8,23 +8,52 @@ class Series extends React.Component {
 
     this.state = {
       series: [],
+      inputFieldValue: "",
+      isFetching: false,
     }
   }
 
   onSeriesInputChange = (event) => {
+    this.setState( { inputFieldValue: event.target.value, isFetching: true } )
     fetch(`http://api.tvmaze.com/search/shows?q=${event.target.value}`)
       .then( (response) => { return( response.json()) })
       .then ( (series) => { this.setState( {
         series: series,
+        isFetching: false,
       } ) })
   }
 
   render() {
+
     return(
       <div>
-        The length of series array - {this.state.series.length}
-        <SeriesList list={this.state.series} />
-        <input onChange={this.onSeriesInputChange}/>
+        <div>
+          <input 
+            type="text"
+            value={this.state.inputFieldValue}
+            onChange={this.onSeriesInputChange}
+          />
+        </div>
+        {
+          this.state.inputFieldValue === "" 
+          &&
+          <p>İzlemek istediğiniz filmin adını yukarıdaki kutucuğa yazınız.</p>
+        }
+        {
+          this.state.inputFieldValue !== "" && this.state.series.length === 0 
+          &&
+          <p>Aradığınız isimde bir film yok, ismi değiştirip tekrar deneyiniz</p>
+        }
+        {
+          this.state.inputFieldValue !== "" && this.state.isFetching 
+          &&
+          <p>Loading......!!!!</p>
+        }
+        {
+          this.state.inputFieldValue !== "" && !this.state.isFetching 
+          &&
+          <SeriesList list={this.state.series} />
+        }
       </div>
     )
   }
