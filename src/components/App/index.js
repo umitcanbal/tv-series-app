f008b2580b481fe30f5e2375629177c86d8e8983
umitcanbal <umitcanbal@gmail.com>
2020-03-21 12:05:48 +0100

    little changes/modifications, nothing important

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Intro from "../Intro"
+import Series from "../../containers/Series"
 
 class App extends Component {
 
@@ -14,28 +15,17 @@ class App extends Component {
       .then( myJson => this.setState( { series: myJson } ) ); 
     
   }
-  
-  // componentDidMount = async () => {
-  //   const url = new URL ("http://api.tvmaze.com/search/shows?q=Vikings");
-  //   console.log("url", url);
-  //   const response = await fetch(url);
-  //   console.log("response", response);
-  //   const json = await response.json();
-  //   console.log("json", json);
-  //   this.setState( { series: json } )
-  // }
-
-  //Yukarıdaki fetching için ikinci bi seçenek
 
   render() {
-    console.log(this.state.series);
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved series"/>
-        The length of series array - {this.state.series.length}
+          The length of series array - {this.state.series.length}
+          <Series />
       </div>
     );
   }
---
+++ src/containers/Series/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Series extends React.Component {
+  render() {
+    return(
+      <div>
+        Series Container
+      </div>
+    )
+  }
+}
+
+export default Series;
